23f84b4dc20999c2675d9bdd11504cb3ed58451f
MattiaPlacido <placido.mattia@gmail.com>
2024-10-23 17:44:17 +0200

    1a milestone

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 //1a parte con solo 2 form e l'output in console
 
 //2a parte con il form nell'immagine stampando i contenuti in pagina
+
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", () => {
+  const km = document.getElementById("km");
+  const age = document.getElementById("fasciaEta");
+
+  let price = 0;
+  const fullprice = km.value * 0.21;
+
+  if (age.value === "1") price = fullprice * 0.8;
+  else if (age.value === "2") price = fullprice;
+  else price = fullprice * 0.6;
+
+  console.log(price);
+});
